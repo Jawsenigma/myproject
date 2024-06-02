44a69fef2184fce5b604758ed9a64f9b09c84778
G-Beast <tanmaysaxena58@gmail.com>
2024-06-02 17:33:01 +0530

    updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'essay_evaluator_db',
+        'USER': 'postgres',
+        'PASSWORD': 'jawerewal',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
