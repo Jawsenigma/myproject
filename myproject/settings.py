1eaae1e67ad0a3ecc492b062c62950dd760902d0
G-Beast <tanmaysaxena58@gmail.com>
2024-06-03 10:32:23 +0530

    updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -165,9 +165,8 @@ AUTHENTICATION_BACKENDS = (
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
