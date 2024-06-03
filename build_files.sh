83569b9e2bb1f4a18dc84c6c0c268ead24f6b22a
G-Beast <tanmaysaxena58@gmail.com>
2024-06-03 10:56:16 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,7 @@
 echo "BUILD START"
 
 python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput 
+python3.12 manage.py collectstatic --noinput --clear
 
-mv staticfiles_build/static /vercel/path0/staticfiles_build/static
 
 echo "BUILD END" 
